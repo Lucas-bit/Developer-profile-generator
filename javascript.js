54777a6da6950139c1f4cdce788919a1bd2cf429
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2020-01-19 13:46:58 -0500

    added bootstrap lay out

--- javascript.js
+++ javascript.js
@@ -3,24 +3,25 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const axios = require("axios")
+const axios = require("axios").default
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
   return inquirer.prompt([{
     message: "Enter your GitHub username",
-    name: "username"
+    name: "username",
   }])
   .then(function({ username, color }) {
-    const queryUrl = `https://api.github.com/search/users?q=${ username }`
+    const queryUrl = `https://api.github.com/search/users?q=${username}`
     return axios.get(queryUrl) 
   })
   .then((res) => {
-      const profileInfo = new Array(res.data.items[0])
+      const profileInfo = res.map(data.items[0])
       console.log(profileInfo)
     })}
+  
 
-     
+     //.name somehwere? .JSON?
 
 
 generateHTML = (res) => {
@@ -30,23 +31,25 @@ generateHTML = (res) => {
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="stylesheet.css">
   <title>Document</title>
 </head>
 <body>
   
+<div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Hi! My name is ${res.data.login}</h1>
+    <h1 class="display-4">Hi! My name is ${res.login}</h1>
     <img = ${res.avatar_url}>
     <p class="lead">My current location is .</p>
-    <p class="lead">I have ${answers.repositories} repositories.</p>
+    <p class="lead">I have ${res.repositories} repositories.</p>
     <p class="lead">I have ${res.followers_url} followers.</p>
     <p class="lead">I have ${res.starred_url} GitHub stars.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <h3><span class="badge badge-secondary">Contact Me</span></h3>
     <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-      <li class="list-group-item">E-mail: ${answers.email}</li>
+      <li class="list-group-item">My GitHub username is ${res.github}</li>
+      <li class="list-group-item">LinkedIn: ${res.linkedin}</li>
+      <li class="list-group-item">E-mail: ${res.email}</li>
     </ul>
   </div>
 </div>
